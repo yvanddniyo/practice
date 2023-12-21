9a5750ee58d6ddc97f643b353c73ec849a3417ef
yvanddniyo <nyvan80076@gmail.com>
2023-12-21 21:48:40 +0200

    question

--- learn.js
+++ learn.js
@@ -17,3 +17,33 @@ const myFilter = (str) => {
 };
 
 console.log(myFilter("hello ! yvan ! i hope !"));
+
+function addFive(inputNumber) {
+  if (typeof inputNumber === "number") {
+    const result = inputNumber + 5;
+    return result;
+  } else {
+    return "Error: Input is not a number";
+  }
+}
+
+const originalNumber = 10;
+const newNumber = addFive(originalNumber);
+console.log(newNumber);
+
+function removeFirstAndLastCharacters(inputString) {
+  // Check if the string has at least two characters
+  if (inputString.length >= 2) {
+    // Use substring to remove the first and last characters
+    const result = inputString.substring(1, inputString.length - 1);
+    return result;
+  } else {
+    // If the string has less than two characters, return an error message or handle it accordingly
+    return "Error: String has less than two characters";
+  }
+}
+
+// Example usage:
+const originalString = "HelloWorld";
+const modifiedString = removeFirstAndLastCharacters(originalString);
+console.log(modifiedString); // Output: elloWorl
