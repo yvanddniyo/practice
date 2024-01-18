085d38425623b5aff9833bc6425ba6f29d1f7161
yvanddniyo <nyvan80076@gmail.com>
2024-01-18 22:17:31 +0200

    functional programming

--- freedcodecamp/algorithm.js
+++ freedcodecamp/algorithm.js
@@ -34,3 +34,116 @@ function chunkArrayInGroups(arr, size) {
   
     return arr.length;
   }
+
+  // Functional Programming Course
+  // INPUT -> OUTPUT -> OUTPUT
+const prepareTea = () => 'greenTea';
+
+const getTea = (numOfCups) => {
+const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
+
+
+                      //CALLBACKS
+// Callbacks are the functions that are slipped or passed into another function to decide the invocation of that function. You may have seen them passed to other methods, for example in filter, the callback function tells JavaScript the criteria for how to filter an array
+
+                    // FRST CLASS fUNCTION
+// Functions that can be assigned to a variable, passed into another function, or returned from another function just like any other normal value, are called first class functions. In JavaScript, all functions are first class functions.
+                  // High order Function
+ // The functions that take a function as an argument, or return a function as a return value, are called higher order functions.
+                  // lambda
+// When functions are passed in to or returned from another function, then those functions which were passed in or returned can be called a lambda.
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTeaa = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTeaa(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTeaa(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+
+// Refactor the global variable in the functions
+
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(arr, bookName) {
+ let newArr = [...arr]
+  newArr.push(bookName);
+  return newArr;
+}
+
+function remove(arr, bookName) {
+  let newArr = [...arr]
+ 
+  if ( newArr.indexOf(bookName) >= 0) {
+
+    newArr.splice(newArr.indexOf(bookName), 1);
+    return newArr;
+
+    }
+}
+
+console.log(remove(['fjfdndv', 'jfvndfjvnfdj', 'hf'], "hf"));
+
+                          //Implement map on a Prototype {how map is build under the hood.}
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+    for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this))
+    }
+  // Only change code above this line
+  return newArray;
+};
+
+// this Function perform the filtering and maping value of the movies
+const ratings  = watchList.filter(rating => rating.imdbRating > 8.0);
+const filteredList  = ratings.map(({Title: title, imdbRating: rating}) => ({title, rating}))
+
+console.log(filteredList)
+
+// how the filter is build
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (Boolean(callback(this[i], i, this)) === true) {
+    newArray.push(this[i])
+    }
+  }
+  return newArray;
+};
