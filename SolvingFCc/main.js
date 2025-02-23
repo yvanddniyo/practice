34833466de5612282b0c63477c6c6dd75ad7ef30
yvanddniyo <nyvan80076@gmail.com>
2025-02-23 18:14:47 +0200

    Practice the problem solving.

--- SolvingFCc/main.js
+++ SolvingFCc/main.js
@@ -29,3 +29,24 @@ const sum2 = res.reduce((a, b) => a + b, 0)
 return sum2 + sum;
 }
 sumAll([1, 4]);
+
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 26
+// Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const twoSum = (nums, target) => {
+  let res = []
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i] + nums[i + 1])
+    if(nums[i] + nums[i + 1] === target) {
+      res.push(i, i + 1)
+    }
+  }
+  return res;
+}
+
+const result = twoSum(nums, target)
+console.log("answers...>", result)
